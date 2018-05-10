d971f856a9e7ae2333d291fff992974354380f70
Thiago Castilho <tvitor.castilho@gmail.com>
2018-05-10 16:34:14 +0000

    Initial Commit

---
+++ OpenFileTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenFileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "openfiles.exe";
+            process.StartInfo.Arguments = "/query /FO CSV /v";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+
+
+            while (true)
+            {
+                try
+                {
+                    process.Start();
+                    if (process.StandardOutput != null)
+                    {
+                        var result = process.StandardOutput.ReadToEnd().Trim().Replace("\"", "");
+                        var lines = result.Split('\n');
+                        var firstLineIndex = 1 + lines.Cast<string>().ToList().FindIndex(l => l.Contains("Hostname"));
+                        for (int i = firstLineIndex; i < lines.Count() && firstLineIndex > 0; i++)
+                        {
+                            var fields = lines[i].Split(',');
+                            foreach (var row in fields)
+                            {
+                                Console.WriteLine(row);
+                            }
+                        }  
+                    }
+                    process.WaitForExit();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" Error: " + e.Message + " : " + DateTime.Now.ToShortTimeString());
+                }
+                finally
+                {
+                    process.Close();
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
